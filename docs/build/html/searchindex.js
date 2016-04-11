26af8ab798f79e384c5950d20fb11031458dbf04
millanp <millan.philipose@gmail.com>
2016-04-11 10:08:17 -0700

    add quickstart page

--- docs/build/html/searchindex.js
+++ docs/build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to dappr&#8217;s documentation!"],titleterms:{dappr:0,document:0,indic:0,tabl:0,welcom:0}})
+Search.setIndex({envversion:47,filenames:["index","quickstart"],objects:{},objnames:{},objtypes:{},terms:{blah:[],content:0,index:0,modul:0,page:0,quickstart:0,search:0},titles:["Welcome to dappr&#8217;s documentation!","Quickstart"],titleterms:{dappr:0,document:0,indic:0,quickstart:1,tabl:0,welcom:0}})
